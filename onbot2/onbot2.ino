5fa458a9d97986645f88e509629f8d2eaa74a540
jonathanw17112532 <17112532@tasfiledomain.tas.edu.tw>
2016-06-16 14:42:44 +0800

    temp pin

--- onbot2/onbot2.ino
+++ onbot2/onbot2.ino
@@ -4,8 +4,8 @@
 #include "OneWire.h"
 #include "DallasTemperature.h"
  
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
+// Data wire is plugged into pin 12 on the Arduino
+#define ONE_WIRE_BUS 12
  
 // Setup a oneWire instance to communicate with any OneWire devices 
 // (not just Maxim/Dallas temperature ICs)
